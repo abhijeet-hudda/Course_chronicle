624a1ccd1171f6c05eca1f6e80b269c1fae10c22
abhijeet-hudda <abhijeethudda9509@gmail.com>
2026-08-14 13:48:20 +0530

    first commit

---
+++ backend/src/utils/constants.js
@@ -0,0 +1,11 @@
+export const DB_NAME = "course_chronicle";
+ 
+// Centralised "magic numbers" so credit logic isn't scattered across controllers
+export const CREDITS = {
+  SIGNUP_BASE: 500,
+  REFERRAL_BONUS: 200,
+  UNLOCK_ANSWER_COST: 5,
+  UPLOAD_REJECTED_PENALTY: 10,
+  UPLOAD_APPROVED_REWARD: 100,
+  PAYMENT_TOPUP: 10000,
+};
